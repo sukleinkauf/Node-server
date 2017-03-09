de2d86f3fc9bec32b0c115b0dc570593764deb2b
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-03-09 15:37:52 -0300

    trocando mensagem

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	res.json({'msg':'Seja bem-vindo!'});
+	res.json({'msg':'Bem-vindo'});
 });
 
 module.exports = router;
