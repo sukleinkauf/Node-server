8c4789e707af959c088f6a0cca7a5196115aab17
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-03-09 15:34:34 -0300

    criando rotas

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var router = express.Router();
 var index = require('./routes/index');
 router.use(index);
 
-var users = require('./routes/users');
-router.use(users);
+var list = require('./routes/list-users');
+router.use(list);
 
-var products = require('./routes/products');
+var products = require('./routes/products');//definindo rotas do meu app
 router.use(products);
+
+var add =require('./routes/add-users');
+router.use(add);
 
 module.exports = router;
