303c04edd66c26351050297c1540c8c420dec4dc
Suelem Kleinkauf <Suelem.Kleinkauf@digitaldesk.com.br>
2017-03-09 13:44:11 -0300

    server application

--- server.js
+++ server.js
@@ -2,5 +2,8 @@ var express = require('express');
 var app = express();
 var port = '3000';
 
+var application = require('')
+app.use(application);
+
 app.listen(port);
 console.log('Servidor iniciado na porta'+port+'Acesse localhost:'+port);
